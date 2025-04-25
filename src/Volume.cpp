3fe5279360abcee4ecd976d03c040b035d42293b
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2025-04-26 00:20:17 +0100

    Determine interaction based on cross-sections using randomness

--- src/Volume.cpp
+++ src/Volume.cpp
@@ -1,21 +1,31 @@
 #include "Volume.hpp"
+#include "MonteCarlo.hpp"
 #include "Constants.hpp"
 
 Interaction Volume::getInteraction(const Particle& p) const {
-    // some random logic to determine what type of interaction occurs
-
-    // get cross-sections for each interaction from the material at the partilce energy
-    // use the cross-sections to determine the probability of each interaction
-    // use a random number to determine which interaction occurs
-    // return the interaction type
-
     const float energy = p.getEnergy(); // MeV
     const float csAbsorb = _material->getCrossSection(energy, Interaction::Absorb);
     const float csScatter = _material->getCrossSection(energy, Interaction::Scatter);
     const float csFission = _material->getCrossSection(energy, Interaction::Fission);
     const float csTotal = csAbsorb + csScatter + csFission; 
 
-    // sample from total cross-seciton, could also be Interaction::None
+    MonteCarlo mc; // TODO: probably should be a member of the world or something rather than constantly making a new one every time
+    const float r = mc.uniform(); // random number between 0 and 1
+
+    const float interaction_distance = -std::log(r) / csTotal; // if cross-section is [cm^-1], distance is in cm
+    const Boundary bound = getNextBoundary(p);
+
+    // Interacts before it leaves this volume
+    if(interaction_distance < bound.distance) {
+        const float pAbsorb = csAbsorb / csTotal;
+        const float pScatter = csScatter / csTotal;
+        const float pFission = csFission / csTotal;
+
+        const float p = mc.uniform(); // random number between 0 and 1
+        if(p < pAbsorb) return Interaction::Absorb;
+        if(p < pAbsorb + pScatter) return Interaction::Scatter;
+        if(p < pAbsorb + pScatter + pFission) return Interaction::Fission;
+    }
 
     return Interaction::None;
 }
